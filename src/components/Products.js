38654b43af599217b6a2cba0f1ee70ac8d02b96e
bryan <bryan.guner@gmail.com>
2021-06-10 08:02:56 -0400

    --- Auto-Git Commit ---

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,40 @@
+// import React from 'react';
+// 
+// // Components
+// import Product from './Product';
+// 
+// const Products = props => {
+// 	return (
+// 		<div className="products-container">
+// 			{props.products.map(product => (
+// 				<Product
+// 					key={product.id}
+// 					product={product}
+// 					addItem={props.addItem}
+// 				/>
+// 			))}
+// 		</div>
+// 	);
+// };
+// 
+// export default Products;
+
+
 import React from 'react';
+import { useContext } from 'react'
+import ProductContext from '../contexts/ProductContext'
 
 // Components
 import Product from './Product';
-
-const Products = props => {
+const Products = () => {
+	const { products, addItem } = useContext(ProductContext)
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
